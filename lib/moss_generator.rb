48c8eb3c49b2688c91f702aba88b9f74bfd094c6
frdrkolsson <fredrik.olsson@standout.se>
2021-01-19 10:05:18 +0100

    Change amount to read from eur

--- lib/moss_generator.rb
+++ lib/moss_generator.rb
@@ -6,6 +6,6 @@ require_relative 'moss_generator/stripe'
 
 # Generate CSV-formatted string for MOSS report
 module MossGenerator
-  Money.default_currency = Money::Currency.new('SEK')
+  Money.default_currency = Money::Currency.new('EUR')
   Money.rounding_mode = BigDecimal::ROUND_HALF_UP
 end
--- spec/moss_generator/stripe_spec.rb
+++ spec/moss_generator/stripe_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe MossGenerator::Stripe do
     it 'returns a csv format string' do
       result = "MOSS_001;\r\n"\
                "SE556000016701;3;2020;\r\n"\
-               "SE;IT;22,00;2038,42;448,45;\r\n"\
-               "SE;FR;20,00;4130,68;826,14;\r\n"\
+               "SE;IT;22,00;205,90;45,30;\r\n"\
+               "SE;FR;20,00;415,00;83,00;\r\n"\
 
       expect(call).to eq(result)
     end
